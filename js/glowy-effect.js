b724fa57e28d6ed93587b032ac91b4ed7feb1c20
midCenturyDude7 <mgriffes@gmail.com>
2025-03-01 22:21:28 -0800

    Half way through tutorial

--- js/glowy-effect.js
+++ js/glowy-effect.js
@@ -1,2 +1,9 @@
 // Event listener for the glow effect on the cards
 // When the mouse hovers over the card, the card will glow
+
+const injectCursorPosition = ({ x, y }) => {
+  document.documentElement.style.setProperty("--x", Math.round(x));
+  document.documentElement.style.setProperty("--y", Math.round(y));
+};
+
+document.body.addEventListener("pointermove", injectCursorPosition);
